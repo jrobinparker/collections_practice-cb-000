e862332f0a6e088efd21039cd9eb822590a914a7
Jeremy Parker <jrobinparker@gmail.com>
2018-06-12 15:27:46 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,4 +1,6 @@
-def sort_array_asc(array + [25, 7, 1])
-  array.sort! { |x, y| x <=> y }
-  puts array 
+def sort_array_asc(array = [25, 7, 1])
+  sorted_array = []
+  array = array.sort! { |x, y| x <=> y }
+  sorted_array << array
+  puts sorted_arrayarray 
 end
